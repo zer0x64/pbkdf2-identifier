d1f9da811f4640a6fcca21a4f65654575a1a6fbd
zer0x64 <dugre.philippe@hotmail.com>
2019-12-08 11:07:39 -0500

    changed input/output types to be more rustic

--- pbkdf2-identifier/src/lib.rs
+++ pbkdf2-identifier/src/lib.rs
@@ -1,13 +1,13 @@
 use std::cmp::min;
 
-#[cfg(not(target_arch="wasm32"))]
+#[cfg(not(target_arch = "wasm32"))]
 use rayon::prelude::*;
 
 use hmac::crypto_mac::generic_array::{sequence::GenericSequence, ArrayLength, GenericArray};
 use hmac::digest::{BlockInput, FixedOutput, Input, Reset};
 use hmac::{Hmac, Mac};
 use sha1::Sha1;
-use sha2::{Sha256, Sha512};
+use sha2::{Sha224, Sha256, Sha384, Sha512};
 
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
@@ -15,7 +15,9 @@ use std::sync::Arc;
 /// A list of the hash algorithms to try
 pub static PRIMITIVES: &'static [HashPrimitive] = &[
     HashPrimitive::HMACSHA1,
+    HashPrimitive::HMACSHA224,
     HashPrimitive::HMACSHA256,
+    HashPrimitive::HMACSHA384,
     HashPrimitive::HMACSHA512,
 ];
 
@@ -25,7 +27,9 @@ pub static PRIMITIVES: &'static [HashPrimitive] = &[
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum HashPrimitive {
     HMACSHA1,
+    HMACSHA224,
     HMACSHA256,
+    HMACSHA384,
     HMACSHA512,
 }
 
@@ -34,19 +38,29 @@ impl HashPrimitive {
     pub fn name(&self) -> &'static str {
         match self {
             HashPrimitive::HMACSHA1 => "HMAC-SHA1",
+            HashPrimitive::HMACSHA224 => "HMAC-SHA224",
             HashPrimitive::HMACSHA256 => "HMAC-SHA256",
+            HashPrimitive::HMACSHA384 => "HMAC-SHA384",
             HashPrimitive::HMACSHA512 => "HMAC-SHA512",
         }
     }
 
     /// Returns a closure for identifying the iteration count for this specific algorithm.
-    pub fn get_identifier(&self) -> Box<dyn Fn(&[u8], &[u8], &[u8], usize, &AtomicBool) -> usize> {
+    pub fn get_identifier(
+        &self,
+    ) -> Box<dyn Fn(&[u8], &[u8], &[u8], Option<usize>, &AtomicBool) -> Option<usize>> {
         match self {
             HashPrimitive::HMACSHA1 => Box::new(|password, hash, salt, max, found| {
                 identify_iterations_threaded::<Sha1>(password, hash, salt, max, found)
             }),
+            HashPrimitive::HMACSHA224 => Box::new(|password, hash, salt, max, found| {
+                identify_iterations_threaded::<Sha224>(password, hash, salt, max, found)
+            }),
             HashPrimitive::HMACSHA256 => Box::new(|password, hash, salt, max, found| {
                 identify_iterations_threaded::<Sha256>(password, hash, salt, max, found)
+            }),
+            HashPrimitive::HMACSHA384 => Box::new(|password, hash, salt, max, found| {
+                identify_iterations_threaded::<Sha384>(password, hash, salt, max, found)
             }),
             HashPrimitive::HMACSHA512 => Box::new(|password, hash, salt, max, found| {
                 identify_iterations_threaded::<Sha512>(password, hash, salt, max, found)
@@ -64,33 +78,25 @@ pub fn identify_all(
     password: &[u8],
     hash: &[u8],
     salt: &[u8],
-    max: usize,
-) -> (HashPrimitive, usize) {
+    max: Option<usize>,
+) -> Option<(HashPrimitive, usize)> {
     let found = Arc::new(AtomicBool::new(false));
     let runner = |primitive: &HashPrimitive| {
-        let (p, i) = (*primitive, primitive.get_identifier()(password, hash, salt, max, &found.clone()));
-        match i {
-            0 => None,
-            _ => Some((p, i))
-        }
+        Some((
+            *primitive,
+            primitive.get_identifier()(password, hash, salt, max, &found.clone())?,
+        ))
     };
 
-    cfg_if::cfg_if!{
+    // WASM doesn't "really" support threads for now, so this operation is not multithreaded in that case
+    cfg_if::cfg_if! {
         if #[cfg(not(target_arch="wasm32"))] {
-            let iter = PRIMITIVES.into_par_iter();
-            if let Some(result) = iter.find_map_any(runner) {
-                return result;
-            }
+            return PRIMITIVES.into_par_iter().find_map_any(runner);
         }
         else {
-            let mut iter = PRIMITIVES.into_iter();
-            if let Some(result) = iter.find_map(runner) {
-                return result;
-            }
+            return PRIMITIVES.into_iter().iter.find_map(runner);
         }
     }
-
-    (HashPrimitive::HMACSHA1, 0)
 }
 
 /// Tries to find the iteration count of the hash knowing its algorithm.
@@ -98,7 +104,12 @@ pub fn identify_all(
 /// hash - The hash itself
 /// salt - The salt used in the derivation
 /// max - The maximum number of iteration to try. Use 0 to try until aborted.
-pub fn identify_iterations<T>(password: &[u8], hash: &[u8], salt: &[u8], max: usize) -> usize
+pub fn identify_iterations<T>(
+    password: &[u8],
+    hash: &[u8],
+    salt: &[u8],
+    max: Option<usize>,
+) -> Option<usize>
 where
     T: Input + BlockInput + FixedOutput + Reset + Default + Clone,
     T::BlockSize: ArrayLength<u8>,
@@ -113,12 +124,18 @@ where
 /// hash - The hash itself
 /// salt - The salt used in the derivation
 /// max - The maximum number of iteration to try. Use 0 to try until aborted.
-/// found - Shared variable to notify other threads if the hash has been identified.  
-pub fn identify_iterations_threaded<T>(password: &[u8], hash: &[u8], salt: &[u8], mut max: usize, found: &AtomicBool) -> usize
-    where
-        T: Input + BlockInput + FixedOutput + Reset + Default + Clone,
-        T::BlockSize: ArrayLength<u8>,
-        T::OutputSize: ArrayLength<u8>,
+/// found - Shared variable to notify other threads if the hash has been identified.
+pub fn identify_iterations_threaded<T>(
+    password: &[u8],
+    hash: &[u8],
+    salt: &[u8],
+    max: Option<usize>,
+    found: &AtomicBool,
+) -> Option<usize>
+where
+    T: Input + BlockInput + FixedOutput + Reset + Default + Clone,
+    T::BlockSize: ArrayLength<u8>,
+    T::OutputSize: ArrayLength<u8>,
 {
     // The accumulator is XORed after each iteration and compared to the value expected
     let mut accumulator = GenericArray::<u8, T::OutputSize>::generate(|_| 0);
@@ -131,9 +148,7 @@ pub fn identify_iterations_threaded<T>(password: &[u8], hash: &[u8], salt: &[u8]
     let mut data = salt.to_vec();
     data.extend_from_slice(&[0, 0, 0, 1]);
 
-    if max == 0 {
-        max = std::usize::MAX;
-    }
+    let max = max.unwrap_or(std::usize::MAX);
 
     for i in 1..max {
         // HMAC the previous result with the hash as the key.
@@ -158,15 +173,15 @@ pub fn identify_iterations_threaded<T>(password: &[u8], hash: &[u8], salt: &[u8]
         // Returns the iteration count if found
         if is_equal {
             found.store(true, Ordering::Relaxed);
-            return i;
+            return Some(i);
         }
 
         if found.load(Ordering::Relaxed) {
-            break;
+            return None;
         }
     }
 
-    0
+    None
 }
 
 #[cfg(test)]
@@ -181,8 +196,9 @@ mod tests {
             "password".as_bytes(),
             &base64::decode("EVeVmsezinX2Nv+J36elk6UA3mJFmJ4Nk2BWHnpbBCg=").unwrap(),
             &base64::decode("akL5xcl1RwlmlfAxkGp1NA==").unwrap(),
-            1000,
-        );
+            None,
+        )
+        .unwrap();
         assert_eq!(iterations, 123);
     }
 
@@ -194,8 +210,9 @@ mod tests {
             "password".as_bytes(),
             &base64::decode("oElyEp3GgQxwdfE6uKfLqz40XB9CTF3iP003JhLPCuc=").unwrap(),
             &base64::decode("RtTNd9YXr4vQxiPQXooELA==").unwrap(),
-            1000,
-        );
+            None,
+        )
+        .unwrap();
         assert_eq!(iterations, 123);
     }
 
@@ -207,8 +224,9 @@ mod tests {
             "password".as_bytes(),
             &base64::decode("Qp+q3JTY/2gnTSRHRNgn3g==").unwrap(),
             &base64::decode("al9TeCB42U/chevUbuaG1w==").unwrap(),
-            200,
-        );
+            None,
+        )
+        .unwrap();
 
         assert_eq!(primitive, HashPrimitive::HMACSHA256);
         assert_eq!(iterations, 125);
