755652ad08603ec73834f8c93736ec291700e810
zer0x64 <dugre.philippe@hotmail.com>
2019-12-08 14:16:45 -0500

    Added default maximum for identify_all

--- pbkdf2-identifier/src/wasm.rs
+++ pbkdf2-identifier/src/wasm.rs
@@ -2,6 +2,8 @@ use wasm_bindgen::prelude::*;
 
 use crate::hash_primitive::HashPrimitive;
 
+static MAX_DEFAULT: usize = 1000000;
+
 #[wasm_bindgen]
 pub struct Pbkdf2Parameters {
     pub primitive: HashPrimitive,
@@ -18,9 +20,9 @@ pub fn identify_all(
     password: &[u8],
     hash: &[u8],
     salt: &[u8],
-    max: usize,
+    max: Option<usize>,
     ) -> Option<Pbkdf2Parameters> {
-    match crate::identify_all(password, hash, salt, Some(max)) {
+    match crate::identify_all(password, hash, salt, Some(max.unwrap_or(MAX_DEFAULT))) {
         None => None,
         Some((primitive, iterations)) => {
             Some(Pbkdf2Parameters {
