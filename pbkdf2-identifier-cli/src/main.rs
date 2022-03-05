2a1aa662acc09d5adc9397ab5dd67030f8f404cd
zer0x64 <dugre.philippe@hotmail.com>
2022-03-05 16:56:47 -0500

    Reworked CLI

--- pbkdf2-identifier-cli/src/main.rs
+++ pbkdf2-identifier-cli/src/main.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 /// Uses pbkdf2-identifier to identify the parameters used to generate a pbkdf2 hash
 use base64;
-use clap::Parser;
+use clap::{ArgEnum, Parser};
 
 #[derive(Debug)]
 struct AlgorithmsArg(Vec<pbkdf2_identifier::hash_primitive::HashPrimitive>);
@@ -23,29 +23,55 @@ impl FromStr for AlgorithmsArg {
     }
 }
 
+#[derive(Debug, Clone, ArgEnum)]
+enum InputFormat {
+    Base64,
+    Hex,
+}
+
+impl InputFormat {
+    fn decode(&self, data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        Ok(match self {
+            Self::Base64 => base64::decode(data)?,
+            Self::Hex => hex::decode(data)?,
+        })
+    }
+}
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
     #[clap(short, long)]
+    /// The cleartext password
     password: String,
 
     #[clap(short, long)]
+    /// The hash of the password to identify
     hash: String,
 
     #[clap(short, long)]
+    /// The salt used to hash the password
     salt: String,
 
-    #[clap(short, long, default_value = "hmacsha1, hmacsha224, hmacsha256, hmacsha384, hmacsha512")]
+    #[clap(
+        short,
+        long,
+        default_value = "hmacsha1, hmacsha224, hmacsha256, hmacsha384, hmacsha512"
+    )]
+    /// The hashing algorithms to try
     algorithms: AlgorithmsArg,
 
-    #[clap(short, long, default_value = "base64")]
-    format: String,
+    #[clap(short, long, arg_enum, default_value = "base64")]
+    /// The encoding format used for the hash and the salt
+    format: InputFormat,
 
     #[clap(short, long)]
+    /// The max number of iterations to try.
+    /// By default there will not be a maximum and the bruteforce will run forever.
     max: Option<usize>,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut primitive_names = vec!["all"];
     for primitive in pbkdf2_identifier::hash_primitive::PRIMITIVES {
         primitive_names.push(primitive.name());
@@ -58,38 +84,8 @@ fn main() {
 
     let format = args.format;
 
-    let hash;
-    let salt;
-
-    if format == "hex" {
-        hash = if let Ok(hash) = hex::decode(hash_enc) {
-            hash
-        } else {
-            println!("Please enter an hex encoded value for the 'hash' parameter");
-            return;
-        };
-
-        salt = if let Ok(salt) = hex::decode(salt_enc) {
-            salt
-        } else {
-            println!("Please enter an hex encoded value for the 'salt' parameter");
-            return;
-        };
-    } else {
-        hash = if let Ok(hash) = base64::decode(&hash_enc) {
-            hash
-        } else {
-            println!("Please enter an base64 encoded value for the 'hash' parameter");
-            return;
-        };
-
-        salt = if let Ok(salt) = base64::decode(&salt_enc) {
-            salt
-        } else {
-            println!("Please enter an base64 encoded value for the 'salt' parameter");
-            return;
-        };
-    }
+    let hash = format.decode(hash_enc.as_bytes())?;
+    let salt = format.decode(salt_enc.as_bytes())?;
 
     match pbkdf2_identifier::identify_algorithms(
         args.password.as_bytes(),
@@ -107,4 +103,6 @@ fn main() {
         }
         None => println!("Not found!"),
     }
+
+    Ok(())
 }
